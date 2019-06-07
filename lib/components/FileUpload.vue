29f8b008ca887d13a12559356ca368d3fd159c79
Tim Mushen <t@x.enterprises>
2019-06-07 13:19:20 -0700

    update paths

--- lib/components/FileUpload.vue
+++ lib/components/FileUpload.vue
@@ -90,8 +90,8 @@ export default {
     return {
       inputModel: null,
       inputModel2: null,
-      imageRegular: require("../../assets/img/image_placeholder.jpg"),
-      imageCircle: require("../../assets/img/placeholder.jpg"),
+      imageRegular: require("../assets/img/image_placeholder.jpg"),
+      imageCircle: require("../assets/img/placeholder.jpg"),
       hasImage: false
     };
   },
@@ -115,9 +115,9 @@ export default {
     },
     removeImage: function() {
       if (this.type === "image-circle") {
-        this.imageCircle = require("../../assets/img/placeholder.jpg");
+        this.imageCircle = require("../assets/img/placeholder.jpg");
       } else {
-        this.imageRegular = require("../../assets/img/image_placeholder.jpg");
+        this.imageRegular = require("../assets/img/image_placeholder.jpg");
       }
       this.hasImage = false;
     },
